056bbd9a54ffaf84c7d6cbabec9e893138aee952
rama8810 <ing.rama88@gmail.com>
2024-05-05 00:27:37 -0500

    new app angular

---
+++ miPag/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, InicioComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'miPag';
+}
